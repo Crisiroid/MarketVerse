b3c944157f095259bd0b61e7448514e453d1256c
Crisiroid <csiroid@gmail.com>
2023-08-25 19:38:23 +0430

    Admin Model added

---
+++ Models/Admin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MarketVerse.Models
+{
+    public class Admin
+    {
+        [Key] public int Id { get; set; }
+        [Required]public int Username { get; set; }
+        [Required]public int Password { get; set; }
+    }
+}
