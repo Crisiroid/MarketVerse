1aac52e31c3ba4359b7f605ba21f5fdb039117c9
Crisiroid <csiroid@gmail.com>
2023-08-25 19:46:35 +0430

    database created and updated

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace MarketVerse.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MarketVerse.Data.MarketVerseContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "MarketVerse.Data.MarketVerseContext";
+        }
+
+        protected override void Seed(MarketVerse.Data.MarketVerseContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
