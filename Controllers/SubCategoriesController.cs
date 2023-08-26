96dc05d271f76f2ca617d8fea83293399a38eac4
Crisiroid <csiroid@gmail.com>
2023-08-26 10:22:05 +0430

    Categories showed on layout

---
+++ Controllers/SubCategoriesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MarketVerse.Data;
+using MarketVerse.Models;
+
+namespace MarketVerse.Controllers
+{
+    public class SubCategoriesController : Controller
+    {
+        private MarketVerseContext db = new MarketVerseContext();
+
+        // GET: SubCategories
+        public ActionResult Index()
+        {
+            return View(db.SubCategories.ToList());
+        }
+
+        // GET: SubCategories/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SubCategory subCategory = db.SubCategories.Find(id);
+            if (subCategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(subCategory);
+        }
+
+        // GET: SubCategories/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SubCategories/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,Name,ItemCount,Code")] SubCategory subCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.SubCategories.Add(subCategory);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(subCategory);
+        }
+
+        // GET: SubCategories/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SubCategory subCategory = db.SubCategories.Find(id);
+            if (subCategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(subCategory);
+        }
+
+        // POST: SubCategories/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,Name,ItemCount,Code")] SubCategory subCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(subCategory).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(subCategory);
+        }
+
+        // GET: SubCategories/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SubCategory subCategory = db.SubCategories.Find(id);
+            if (subCategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(subCategory);
+        }
+
+        // POST: SubCategories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            SubCategory subCategory = db.SubCategories.Find(id);
+            db.SubCategories.Remove(subCategory);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Models/SubcategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using MarketVerse.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MarketVerse.Models
+{
+    public class SubcategoryRepository
+    {
+        public static MarketVerseContext db = new MarketVerseContext();
+
+        public static List<SubCategory> ShowAllSubCategories()
+        {
+            return db.SubCategories.ToList();
+        }
+
+    }
+}
