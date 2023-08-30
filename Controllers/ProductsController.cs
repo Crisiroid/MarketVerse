11c5bd8564cf463a09e72e40e4eb1ca060302b12
Crisiroid <csiroid@gmail.com>
2023-08-30 18:08:58 +0430

    Product Controller Added

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,34 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
-using System.Linq;
-using System.Net;
-using System.Web;
+﻿using System.Net;
 using System.Web.Mvc;
-using MarketVerse.Data;
 using MarketVerse.Models;
 
 namespace MarketVerse.Controllers
 {
     public class ProductsController : Controller
     {
-        private MarketVerseContext db = new MarketVerseContext();
-
-        // GET: Products
         public ActionResult Index()
         {
-            return View(db.Products.ToList());
+            if (TempData["pm"]!= null)
+            {
+                ViewBag.pm = TempData["pm"].ToString();
+                TempData.Clear();
+            }
+            return View(Product.ShowAllProducts());
         }
 
-        // GET: Products/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Product product = db.Products.Find(id);
+            Product product = Product.FindProduct((int)id);
             if (product == null)
             {
                 return HttpNotFound();
@@ -36,37 +30,34 @@ namespace MarketVerse.Controllers
             return View(product);
         }
 
-        // GET: Products/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Products/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "id,Name,Description,Price,Quantity,SubCategoryid,Purchuses,Views")] Product product)
+        public ActionResult Create(Product product)
         {
-            if (ModelState.IsValid)
+            if (Product.Create(product))
             {
-                db.Products.Add(product);
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                TempData["pm"] = "Product Added";
+                return RedirectToAction("Index", "Products");
             }
-
-            return View(product);
+            else
+            {
+                TempData["pm"] = "Something went wrong!";
+                return RedirectToAction("Create", "Products");
+            }
         }
 
-        // GET: Products/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Product product = db.Products.Find(id);
+            Product product = Product.FindProduct((int)id);
             if (product == null)
             {
                 return HttpNotFound();
@@ -74,30 +65,29 @@ namespace MarketVerse.Controllers
             return View(product);
         }
 
-        // POST: Products/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "id,Name,Description,Price,Quantity,SubCategoryid,Purchuses,Views")] Product product)
+        public ActionResult Edit(Product product)
         {
-            if (ModelState.IsValid)
+            if (Product.Edit(product))
             {
-                db.Entry(product).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                TempData["pm"] = "Product Updated Successfully!";
+                return RedirectToAction("Index", "Product");
             }
-            return View(product);
+            else
+            {
+                TempData["pm"] = "Something went wrong";
+                return RedirectToAction("Index", "Product");
+            }
         }
 
-        // GET: Products/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Product product = db.Products.Find(id);
+            Product product = Product.FindProduct((int)id);
             if (product == null)
             {
                 return HttpNotFound();
@@ -105,24 +95,20 @@ namespace MarketVerse.Controllers
             return View(product);
         }
 
-        // POST: Products/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            Product product = db.Products.Find(id);
-            db.Products.Remove(product);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
+            if (Product.Delete(id))
             {
-                db.Dispose();
+                TempData["pm"] = "Product Deleted Successfully!";
             }
-            base.Dispose(disposing);
+            else
+            {
+                TempData["pm"] = "Something went wrong";
+            }
+            return RedirectToAction("Index", "Products");
+
         }
     }
 }
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
@@ -18,5 +19,59 @@ namespace MarketVerse.Models
         [Required] public int SubCategoryid { get; set; }
         [Required] public int Purchuses { get; set; }
         [Required] public int Views { get; set; } = 0;
+
+        public static List<Product> ShowAllProducts()
+        {
+            return DatabaseModel.db.Products.ToList();
+        }
+
+        public static Product FindProduct(int id)
+        {
+            return DatabaseModel.db.Products.FirstOrDefault(x => x.id == id);
+        }
+
+        public static bool Create(Product product)
+        {
+            try
+            {
+                product.Purchuses = 0;
+                product.Views = 0;
+                DatabaseModel.db.Products.Add(product);
+                DatabaseModel.db.SaveChanges();
+                return true;
+            }catch(Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public static bool Edit(Product product)
+        {
+            try
+            {
+                DatabaseModel.db.Entry(product).State = EntityState.Modified;
+                DatabaseModel.db.SaveChanges();
+                return true;
+            }catch(Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public static bool Delete(int id)
+        {
+            try
+            {
+                Product product = FindProduct(id);
+                DatabaseModel.db.Products.Remove(product);
+                DatabaseModel.db.SaveChanges();
+                return true;
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }
+            
+        }
     }
 }
