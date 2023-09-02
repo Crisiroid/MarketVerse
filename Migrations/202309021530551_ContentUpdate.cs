2380f99577fc0d62e67838961bcb007865c7a425
Crisiroid <csiroid@gmail.com>
2023-09-02 20:06:01 +0430

    Filled AddContent Page

---
+++ Migrations/202309021530551_ContentUpdate.cs
@@ -0,0 +1,18 @@
+﻿namespace MarketVerse.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ContentUpdate : DbMigration
+    {
+        public override void Up()
+        {
+            RenameTable(name: "dbo.Contents", newName: "Posts");
+        }
+        
+        public override void Down()
+        {
+            RenameTable(name: "dbo.Posts", newName: "Contents");
+        }
+    }
+}
