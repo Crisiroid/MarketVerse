fe8b3177f10cc98c8d9b1c2ee7055f88d1421025
Crisiroid <csiroid@gmail.com>
2023-09-02 19:44:17 +0430

    Bug fix and added content management Items

---
+++ Controllers/ContentsController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MarketVerse.Data;
+using MarketVerse.Models;
+
+namespace MarketVerse.Controllers
+{
+    public class ContentsController : Controller
+    {
+        private MarketVerseContext db = new MarketVerseContext();
+        public ActionResult Index()
+        {
+            return View(db.Contents.ToList());
+        }
+
+        // GET: Contents/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Content content = db.Contents.Find(id);
+            if (content == null)
+            {
+                return HttpNotFound();
+            }
+            return View(content);
+        }
+
+        // GET: Contents/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Contents/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,ProductId,Title,Text")] Content content)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Contents.Add(content);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(content);
+        }
+
+        // GET: Contents/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Content content = db.Contents.Find(id);
+            if (content == null)
+            {
+                return HttpNotFound();
+            }
+            return View(content);
+        }
+
+        // POST: Contents/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,ProductId,Title,Text")] Content content)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(content).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(content);
+        }
+
+        // GET: Contents/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Content content = db.Contents.Find(id);
+            if (content == null)
+            {
+                return HttpNotFound();
+            }
+            return View(content);
+        }
+
+        // POST: Contents/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Content content = db.Contents.Find(id);
+            db.Contents.Remove(content);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
