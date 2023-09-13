2f13adf09241c9c93ae83d316eb22b244601e230
Crisiroid <csiroid@gmail.com>
2023-09-13 21:17:40 +0430

    Update: Improved UI Added Slide Show Changed Colors

---
+++ Migrations/202309131618189_updateCategory.cs
@@ -0,0 +1,18 @@
+﻿namespace MarketVerse.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class updateCategory : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Categories", "SubcategoryCount", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Categories", "SubcategoryCount", c => c.String(nullable: false));
+        }
+    }
+}
