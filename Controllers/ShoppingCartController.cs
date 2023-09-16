873cbe5b12d7185bc2ca0a80477171f2e5e4f9f2
Crisiroid <csiroid@gmail.com>
2023-09-16 18:23:19 +0430

    Update: Added methods for Adding and Removing Items from cart Updated the ShowProduct View to add the Items

--- Controllers/ShoppingCartController.cs
+++ Controllers/ShoppingCartController.cs
@@ -15,9 +15,44 @@ namespace MarketVerse.Controllers
         {
             cartItems = new List<CartItem>();
         }
-        public ActionResult Cart()
+        public ActionResult Index()
         {
-            return View();
+            return View(cartItems);
+        }
+
+        public ActionResult AddToCart(int productId, string productName, decimal price, int quantity)
+        {
+            var existingItem = cartItems.FirstOrDefault(item => item.ProductId == productId);
+
+            if (existingItem != null)
+            {
+                existingItem.Quantity += quantity;
+            }
+            else
+            {
+                cartItems.Add(new CartItem
+                {
+                    ProductId = productId,
+                    ProductName = productName,
+                    Price = price,
+                    Quantity = quantity
+                });
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult RemoveFromCart(int productId)
+        {
+            var itemToRemove = cartItems.FirstOrDefault(item => item.ProductId == productId);
+
+            if (itemToRemove != null)
+            {
+                cartItems.Remove(itemToRemove);
+            }
+
+            // Redirect back to the shopping cart page
+            return RedirectToAction("Index");
         }
     }
 }
