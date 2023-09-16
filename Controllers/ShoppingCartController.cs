bc2d7365a8bc3b534b175d2dbf7352060247c77f
Crisiroid <csiroid@gmail.com>
2023-09-16 18:14:15 +0430

    Created: Shopping cart Model and Controller

---
+++ Controllers/ShoppingCartController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MarketVerse.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        public ActionResult Cart()
+        {
+            return View();
+        }
+    }
+}
