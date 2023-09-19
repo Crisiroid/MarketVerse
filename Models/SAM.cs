7c9bbe659dcfecf90930163e98c10c1c348dc813
Crisiroid <csiroid@gmail.com>
2023-09-19 21:33:38 +0430

    Update: Added SAM(Site Activity months)

---
+++ Models/SAM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MarketVerse.Models
+{
+    public class SAM
+    {
+        //Site Activity Month
+        [Key] public int Id { get; set; }
+        [Required] public string Name { get; set; }
+        [Required] public int SaleNum { get; set; }
+        [Required] public int TotalProfit { get; set; }
+    }
+}
